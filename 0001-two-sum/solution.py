faac60d508933dd7a4adc270ffed752694210cb9
Dushyant Jadon <dushyantsinghjadon1712@gmail.com>
2026-08-28 01:28:11 +0530

    Fix: add actual solution code for Two Sum

---
+++ 0001-two-sum/solution.py
@@ -0,0 +1,15 @@
+"""
+   1. Two Sum
+   https://leetcode.com/problems/two-sum/
+   Difficulty: Easy
+   """
+
+class Solution:
+   def twoSum(self, nums: list[int], target: int) -> list[int]:
+       seen = {}
+       for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+               return [seen[complement], i]
+            seen[num] = i
+       return []
